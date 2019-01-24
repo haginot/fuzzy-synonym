a1e290fc56fbf3240b454151f55a9e6107e12174
Hagino Takahiro <hagino@apache.org>
2019-01-24 21:59:24 +0900

    Add setup file for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name="fuzzy-synonym",
+    version="0.0.1",
+    author="Hagino Takahiro",
+    author_email="haginota@gmail.com",
+    description=("Automated Synonym Aggregation."),
+    license="Apache Software License",
+    keywords="synonym",
+    url="https://github.com/haginot/fuzzy-synonym",
+    packages=['fuzzy_synonym'],
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    install_requires=['numpy', 'pandas'],
+    classifiers=[]
+)
