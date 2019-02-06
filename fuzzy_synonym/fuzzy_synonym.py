3741808a138cb7c93bcb587947806c325596b49a
Hagino Takahiro <hagino@apache.org>
2019-02-06 22:47:47 +0900

    To make FuzzySynonym class take fixed_tags and create instance of TagDict in initing

--- fuzzy_synonym/fuzzy_synonym.py
+++ fuzzy_synonym/fuzzy_synonym.py
@@ -1,4 +1,11 @@
+from fuzzy_synonym.tag_dict import TagDict
+
+
 class FuzzySynonym:
+    def __init__(self,
+                 fixed_tags = []):
+        tag_dict = TagDict(fixed_tags)
+
     def aggregate(self,
                   fixed_tags,
                   observed_tags):
